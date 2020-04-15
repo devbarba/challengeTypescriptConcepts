de9eb73d0bd0a86cfa1c1db713bee4fd74bc4dcb
João Harbs <harbsprog@gmail.com>
2020-04-14 23:35:24 -0300

    Finishing typescript transactions app

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  private calculateBalance(operator: string): number {
+    return this.transactions
+      .filter(t => t.type === operator)
+      .reduce((acc, tr) => acc + tr.value, 0);
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.calculateBalance('income');
+    const outcome = this.calculateBalance('outcome');
+    const total = income - outcome;
+
+    const balance: Balance = { income, outcome, total };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    const { total } = this.getBalance();
+    if (type === 'outcome' && total < value) {
+      throw Error('You cannot remove more than you have in the box.');
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
